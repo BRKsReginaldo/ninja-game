18e0fb2654486c735d14bd83112da89b05953e8e
Reginaldo Junior <reginaldo.junior696@gmail.com>
2020-04-13 22:01:39 -0300

    gitploy

---
+++ precache-manifest.c48ce05054baa7efd0053e75b46d3c9b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbe6c8f065ad3411ce05885f058f5ea3",
+    "url": "https://brksreginaldo.github.io/ninja-game/index.html"
+  },
+  {
+    "revision": "73e30efb989469cdf027",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/css/main.a421fb84.chunk.css"
+  },
+  {
+    "revision": "3cfaa3bfd7b0d454730c",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/js/2.4a6da4f1.chunk.js"
+  },
+  {
+    "revision": "94dfdc41096e9610266ccd6f07e3c278",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/js/2.4a6da4f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "73e30efb989469cdf027",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/js/main.ec60e626.chunk.js"
+  },
+  {
+    "revision": "2d3dc76bbab5dfffc5f7",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/js/runtime-main.34737156.js"
+  },
+  {
+    "revision": "abe089e8dc19408bd675c169e9cb7ad2",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/background.abe089e8.png"
+  },
+  {
+    "revision": "9aade6d7e5e98dfd0051e932510d4f69",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/click.9aade6d7.ogg"
+  },
+  {
+    "revision": "df32ce998e0cd19c3106baeba401b01f",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/click.df32ce99.mp3"
+  },
+  {
+    "revision": "7066785b2f0736541c691e662391baae",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/clock.7066785b.png"
+  },
+  {
+    "revision": "b3008311d087f5f9d3806791a1df58e9",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/cloud.b3008311.png"
+  },
+  {
+    "revision": "95534270a0e18269cf52f6a52ea879d6",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/dangertile.95534270.png"
+  },
+  {
+    "revision": "21dc985c247516414e210c5711f819f0",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/death.21dc985c.ogg"
+  },
+  {
+    "revision": "61ed9858a5da76d7a88c4da820eb203d",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/death.61ed9858.mp3"
+  },
+  {
+    "revision": "3e4fcd2c6f369032d22e75e0737dbc5b",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/energybar.3e4fcd2c.png"
+  },
+  {
+    "revision": "4d6c771cedf72f7ca49e5313e9817ffe",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/font.4d6c771c.fnt"
+  },
+  {
+    "revision": "b8d1bfa95668c8715d3d0da52a942e53",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/font.b8d1bfa9.png"
+  },
+  {
+    "revision": "69df2205aa18b8a17c5eebccf2e453ba",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/grow.69df2205.mp3"
+  },
+  {
+    "revision": "e21a5ce0a9240858e5fcbd18d0fee706",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/grow.e21a5ce0.ogg"
+  },
+  {
+    "revision": "31ab953b15e0f81b6e0898d1f4faee12",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/hero.31ab953b.png"
+  },
+  {
+    "revision": "a649144320f3277b51a59c0451843305",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/icons.a6491443.png"
+  },
+  {
+    "revision": "3d93ece348a47845de76022b305179a6",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/info.3d93ece3.png"
+  },
+  {
+    "revision": "21a49f79b46d61a157ff756ecb72d2cc",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/pick.21a49f79.ogg"
+  },
+  {
+    "revision": "40fdd5dd0a613ec76c8a88116b884c06",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/pick.40fdd5dd.mp3"
+  },
+  {
+    "revision": "20b82413d48873672802255c6053323e",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/playbutton.20b82413.png"
+  },
+  {
+    "revision": "79a85315eeb9fcb93fbc74cd2f4865e3",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/run.79a85315.mp3"
+  },
+  {
+    "revision": "e969c4633ea77044278a0b29d85f521b",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/run.e969c463.ogg"
+  },
+  {
+    "revision": "41b72b88fbfda29133063404791bc482",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/stick.41b72b88.ogg"
+  },
+  {
+    "revision": "42a3155103a9b9ea126bb74a8dff653d",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/stick.42a31551.mp3"
+  },
+  {
+    "revision": "c9514dd3d31da367dbfcfd457a2d4260",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/tile.c9514dd3.png"
+  },
+  {
+    "revision": "37e7f2b3c6925b0a78b955ccfa865cdf",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/title.37e7f2b3.png"
+  },
+  {
+    "revision": "f6f948be31613ecd750107cbd56acaa7",
+    "url": "https://brksreginaldo.github.io/ninja-game/static/media/whitetile.f6f948be.png"
+  }
+]);
